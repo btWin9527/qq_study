8334614c88be52d2d4da2db1560627317caae533
郭吉光 <18434392328@163.com>
2019-12-07 17:37:46 +0800

    登录模块解析

--- 04-node/01-expressTest/app/controllers/user.ctrl.js
+++ 04-node/01-expressTest/app/controllers/user.ctrl.js
@@ -3,7 +3,7 @@ import logger from "../util/logger";
 import * as userService from '../services/user.service'
 
 const operations = {
-  // 登录接口
+  // 登录接口   义务逻辑--记录登录用户信息session
   login: function (req, res) {
     let {account, password} = req.body;
     logger.info('====>login...' + account + ',' + password);
@@ -11,6 +11,7 @@ const operations = {
       .then((data) => {
         console.log(data);
         if (data) {
+          // req.session =
           res.status(200).json(data);
         } else {
           res.status(404).send('读取数据失败');
