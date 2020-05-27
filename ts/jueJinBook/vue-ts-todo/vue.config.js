69e53ef60c3c9ad52b4295e3f5f3e80350a780ad
郭吉光 <18434392328@163.com>
2020-05-27 18:32:55 +0800

    掘金小册ts学习 -- vue项目配置ts

---
+++ ts/jueJinBook/vue-ts-todo/vue.config.js
@@ -0,0 +1,33 @@
+/*
+ * 优化处理：vant按需引入
+ * */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const merge = require("webpack-merge");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tsImportPluginFactory = require("ts-import-plugin");
+module.exports = {
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.module
+      .rule("ts")
+      .use("ts-loader")
+      .tap(options => {
+        options = merge(options, {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: "vant",
+                libraryDirectory: "es",
+                style: true
+              })
+            ]
+          }),
+          compilerOptions: {
+            module: "es2015"
+          }
+        });
+        return options;
+      });
+  }
+};
