2f26302a9cda76eb67f2ff0a08e5857a8c4569ef
郭吉光 <18434392328@163.com>
2020-09-11 15:02:23 +0800

    react概念添加

---
+++ reactStudy/officeDoc/01-核心概念/js/index.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+
+  // 计时器组件
+/*  class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        date: new Date()
+      }
+    }
+
+
+    componentDidMount() {
+      this.timerID = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID)
+    }
+
+    tick() {
+      this.setState({
+        date: new Date()
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <p>It is {this.state.date.toLocaleTimeString()}</p>
+        </div>
+      )
+    }
+  }
+
+  ReactDOM.render(<Clock/>, document.getElementById('root'));*/
+
+  // 切换开关
+
+}
