5d34c58831edb9d5e39d0f0dd7d7cdec4d17dde5
郭吉光 <18434392328@163.com>
2020-11-24 16:06:16 +0800

    feat(view): umijs密码配置

--- reactStudy/umijsTest/src/constant/enumConfig.js
+++ reactStudy/umijsTest/src/constant/enumConfig.js
@@ -1,6 +1,8 @@
 import createEnum from "../utils/createEnum";
 
-export default Object.freeze({})
+export default Object.freeze({
+  "signature": "E6AgEAAkEAno2nAMc3b", // 密码加签
+})
 
 // 国家枚举
 export const COUNTRY_CONFIG = createEnum({
