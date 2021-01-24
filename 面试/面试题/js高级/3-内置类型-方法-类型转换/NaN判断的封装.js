70aa0dd8ed633620324d8403c9b7830a48dbded7
guojiguang <1135899486@qq.com>
2021-01-24 19:40:03 +0800

    feat(data): react_jsx学习             目录清空无用项目

--- 面试/面试题/js高级/3-内置类型-方法-类型转换/NaN判断的封装.js
+++
@@ -1,7 +0,0 @@
-// ES6兼容写法
-
-if (!Number.isNaN) {
-  Number.isNaN = function (num) {
-    return Number(num) && window.isNaN(num);
-  }
-}
--- 面试/面试题/js高级/3-内置类型-方法-类型转换/is0.js
+++
@@ -1,7 +0,0 @@
-/* 正0和负0的判断*/
-function isminus0(num) {
-  return num === 0 && (1 / num === -infinity);
-}
-
-/* 判断正0和负0*/
-console.log(Object.is(0,-0));
